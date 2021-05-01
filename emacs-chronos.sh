2813f3a8e537f14b8837bdaa622c40888c802a3f
Evgeny Boykov <artscan@list.ru>
2021-05-01 18:48:57 +1000

    add eab-emacs-chronos mvp fast bootstrap

---
+++ emacs-chronos.sh
@@ -0,0 +1,21 @@
+docker run \
+-e UNAME="eab" \
+-e GNAME="eab" \
+-e UID="1001" \
+-e GID="1001" \
+-e UHOME="/home/eab" \
+-e LANG="ru_RU.UTF-8" \
+-e LC_ALL="en_US.UTF-8" \
+-e TZ="Asia/Vladivostok" \
+-v $('pwd'):/mnt/workspace \
+-v /home/eab/.emacs.d:/home/eab/.emacs.d \
+-v /home/eab/.ssh:/home/eab/.ssh \
+-v /etc/localtime:/etc/localtime:ro \
+-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=$DISPLAY \
+--device /dev/console:/dev/console:rw \
+--privileged \
+-h emacs-chronos \
+--name eab-emacs-chronos \
+ eab-emacs28
